a45a77607968501583c96d1346c0f55badbe193a
Varun-2538 <942525@gmail.com>
2024-12-07 13:41:11 +0530

    added button login

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,6 +11,11 @@ export default function Navbar() {
     setShowNavbar(!showNavbar);
   };
 
+  const handleLogin = () => {
+    // Add logic for login here
+    console.log("Login button clicked");
+  };
+
   return (
     <nav className="w-9/12 xl:w-11/12 2xl:w-9/12 z-50 h-16 rounded-[60px] px-6 py-4 xl:py-2 bg-white backdrop-blur-xl shadow-xl border-stone-50 border-1 mx-auto mt-4 mb-16 fixed left-1/2 -translate-x-1/2">
       <div className="flex justify-between items-center h-full">
@@ -43,12 +48,16 @@ export default function Navbar() {
           </li>
           <li>
             <CustomButton
-                primaryText={walletAddress ? "Connected" : "Connect Wallet"}
-                onClick={connectWallet}
+              primaryText={walletAddress ? "Connected" : "Connect Wallet"}
+              onClick={connectWallet}
             />
-
           </li>
-          
+          <li>
+            <CustomButton
+              primaryText="Login"
+              onClick={handleLogin}
+            />
+          </li>
           <li>
             <div className="bg-divider h-2/3 mx-6 hidden max-[1024px]:hidden"></div>
           </li>
