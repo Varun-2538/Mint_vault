5b81070f8e99f31338427dab6126b092a8f1c108
Milind <milindmishra11@gmail.com>
2024-12-08 09:26:38 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,8 @@ app.get("/crypto-prices", async (req, res) => {
 
 	// Load environment variables for Ethereum
 	const RPC_URL = process.env.RPC_URL || "https://rpc-amoy.polygon.technology/"; // Replace with your desired RPC URL
-	const PRIVATE_KEY =
-	process.env.PRIVATE_KEY ||
-	; // Ensure this is securely stored
+	const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+	// Ensure this is securely stored
 	const CONTRACT_ADDRESS =
 	process.env.CONTRACT_ADDRESS || "0x1b675F19c627b85D6C486109A726C68410840934"; // Replace with your deployed contract address
 
